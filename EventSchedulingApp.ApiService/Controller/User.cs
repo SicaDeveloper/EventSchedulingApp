956aafb6e7734792f2d2f7f81a599ab3bdd23f9e
Sica <rajdangol.dev@gmail.com>
2025-07-10 07:29:12 +0545

    commit
    Signed-off-by: Sica <rajdangol.dev@gmail.com>

---
+++ EventSchedulingApp.ApiService/Controller/User.cs
@@ -0,0 +1,11 @@
+﻿namespace EventSchedulingApp.ApiService.Controller;
+
+public class User
+{
+    public required Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Username {get; set;}
+    public required string Email { get; set; }
+    public required string HashedPassword { get; set; }
+    
+}   
---
+++ EventSchedulingApp.ApiService/Controller/UsersController.cs
@@ -0,0 +1,9 @@
+﻿using EventSchedulingApp.ApiService.Controller;
+
+namespace EventSchedulingApp.ApiService.Controller;
+
+public class UsersController
+{
+    private List<User> _users = new List<User>();
+    
+}
